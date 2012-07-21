cc24f806bd30de62654e1a7ce3e7ebd1d9b5b565
Vojto Rinik <vojto@rinik.net>
2012-07-21 14:45:28 -0700

    Initial commit for iOS app

---
+++ ios/Lifecraft/LCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LCAppDelegate.h
+//  Lifecraft
+//
+//  Created by Rinik Vojto on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/Lifecraft/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lifecraft
+//
+//  Created by Rinik Vojto on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LCAppDelegate class]));
+    }
+}
