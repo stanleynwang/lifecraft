c40fdb6c1987009ba10c050caa1f95cac8fd636c
Stanley Wang <stanleynwang@gmail.com>
2012-07-22 06:08:34 -0700

    Merge

--- lifecraft_backend/app/models/quest.rb
+++ lifecraft_backend/app/models/quest.rb
@@ -1,5 +1,17 @@
+<<<<<<< Updated upstream
 class Quest < ActiveRecord::Base
   belongs_to :user
   # has_one :user
   belongs_to :activity
+=======
+class Quest
+  attr_accessor :user, :activity
+
+  def to_json
+    {
+      :user_id => @user.id,
+      :activity_id => @activity.id
+    }.to_json
+  end
+>>>>>>> Stashed changes
 end
